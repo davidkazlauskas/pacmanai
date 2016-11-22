3244a7613d3967ecf2a23bedcc68fb5e5c6dee4b
David <david@templatious.org>
2016-11-22 18:53:41 +0200

    visualize pecmen state

--- src/pacman/core.cljs
+++ src/pacman/core.cljs
@@ -1,5 +1,7 @@
 (ns pacman.core
-  (:require ))
+  (:require [pacman.ai :as a]
+            [hiccups.runtime :as hiccupsrt])
+  (:require-macros [hiccups.core :as hiccups :refer [html]]))
 
 (enable-console-print!)
 
@@ -7,9 +9,50 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Hello world boo!"}))
+
+(defn cell-id [x y]
+  (str "cell-" x "-" y))
+
+(defn prep-pac-table [w h]
+  (html [:table
+         (map (fn [y]
+                (html [:tr
+                      (map (fn [x] (html
+                                     [:td {:id (cell-id x y)
+                                           :style "width: 30px; height: 30px;"}]
+                                     )) (range w))
+                      ])) (range h))
+         ]))
+
+(defn css-class-for-type [el-type]
+  ({
+   :wall "wall"
+   :bean "bean"
+   :pacman "pacman"
+   :ghost "ghost"
+   :space "space"
+   } el-type))
+
+(defn set-cell-for-type [el-type x y]
+  (set! (.-className (.getElementById js/document (cell-id x y))) (css-class-for-type el-type)))
+
+(defn render-pacman-table [the-str width]
+  (let [dat (a/str-map-2-data the-str width)]
+    (dorun
+     (map-indexed
+      (fn [y row]
+        (dorun
+          (map-indexed
+            (fn [x elem]
+              (set-cell-for-type (:type elem) x y))
+            row)))
+      dat))))
 
 (defn on-js-reload []
+  (set! (.-innerHTML (.getElementById js/document "app"))
+        (prep-pac-table 19 21))
+  (render-pacman-table a/sample-map 19)
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
